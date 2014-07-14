eeb445d91f49fb0bddee76744d85cd3496bba3c8
Ilya Murav'jov <muravev@yandex.ru>
2014-07-14 09:24:38 +0400

    loading from memory & fixes

--- ssh-inet.py
+++ ssh-inet.py
@@ -3,10 +3,9 @@
 
 import os
 import struct
-
-import tornado.ioloop
-import tornado.iostream
 import socket
+import sys
+import argparse
 
 #######################
 # from 
@@ -43,6 +42,54 @@ def pyiface_open_tun_interface():
     name = "tun3"
     ifr.ifr_name = ifr.ifr_name.__class__(bytes(name, "ascii"))
     ifr.ifr_flags = IFF_TUN
+
+#######################
+
+def parse_args():
+    self_options, ssh_options = [], sys.argv[1:]
+    try:
+        idx = ssh_options.index("--")
+        self_options, ssh_options = ssh_options[:idx], ssh_options[idx+1:]
+    except ValueError:
+        pass
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        '-v', '--verbose', default=False,
+        action='store_true',
+    )
+    
+    parser.add_argument(
+        '--remote_port', default=None,
+        type=int, 
+        help='remote client mode',
+    )
+    
+    parser.add_argument(
+        '--remote_python_binary', default=None,
+        help='remote tornado pythonpath',
+    )
+    
+    return parser.parse_args(self_options), ssh_options
+
+args, ssh_options = parse_args()
+is_local = args.remote_port is None
+
+if args.verbose:
+    def log(msg, *msgs):
+        prefix = "local:" if is_local else "remote:"
+        print(prefix, msg, *msgs)
+else:
+    def log(msg, *msgs):
+        pass
+
+if is_local and not ssh_options:
+    log("no server to connect")
+    sys.exit(1)
+
+#import tornado.ioloop
+#import tornado.iostream
 
 #######################
 
@@ -85,33 +132,16 @@ def connect(host, port, callback):
         callback(None)
     stream.set_close_callback(on_close)
 
-import argparse
 def make_struct(**kwargs):
     return argparse.Namespace(**kwargs)
 
 import subprocess
 import shlex
 def call_cmd(cmd):
-    #print(cmd)
+    log(cmd)
     return subprocess.call(shlex.split(cmd))
 
 def main():
-    import sys
-    
-    self_options, ssh_options = [], sys.argv[1:]
-    try:
-        idx = ssh_options.index("--")
-        self_options, ssh_options = ssh_options[:idx], ssh_options[idx+1:]
-    except ValueError:
-        pass
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--remote_port', default=None,
-        type=int, 
-        help='remote client mode',
-    )
-    args = parser.parse_args(self_options)
     
     # чтобы без root'а запускать: sudo setcap cap_net_admin+eip
     tun, ifname = open_tun_interface()
@@ -119,7 +149,6 @@ def main():
     local_ip  = "10.0.0.1"
     remote_ip = "10.0.0.2"
 
-    is_local = args.remote_port is None
     this_ip = local_ip if is_local else remote_ip
     that_ip = remote_ip if is_local else local_ip
         
@@ -147,19 +176,19 @@ def main():
         )
         
         def on_stream_end():
-            print("Stream is closed")
+            log("Stream is closed")
             io_loop.stop()
         
         # 1 тоннель
         def write_to_socket(bs):
             if bs:
-                print("tun => socket")
+                log("tun => socket")
                 
                 strm = tun_socket.socket
                 if strm:
                     strm.write(bs)
                 else:
-                    print("No socket, dropping")
+                    log("No socket, dropping")
         
         def handle_tun_read(bs):
             write_to_socket(bs)
@@ -172,7 +201,7 @@ def main():
         
         # 2 сокет
         def write_to_tun(bs):
-            print("socket => tun")
+            log("socket => tun")
             
             if bs:
                 tun_socket.tun.write(bs)
@@ -192,9 +221,41 @@ def main():
                 if tun_socket.socket is None:
                     start_proxying(stream)
                 else:
-                    print("Needless connection, dropping")
+                    log("Needless connection, dropping")
                     stream.close()
-            start_tcp_server(handle_stream, 1080)
+            port = 1080
+            start_tcp_server(handle_stream, port)
+            
+            # меняем назад uid, если под sudo, чтобы не смутить ssh
+            if "SUDO_UID" in os.environ:
+                os.setresuid(int(os.environ["SUDO_UID"]), -1, -1)
+                # странно, почему-то ssh вылетал с ошибкой "нельзя выполнить setresgid()"
+                # теперь вот перестал
+                #orig_gid = int(os.environ["SUDO_GID"])
+                #os.setresgid(orig_gid, orig_gid, orig_gid)
+            
+            # запускаем удаленно скрипт
+            python_exec = args.remote_python_binary if args.remote_python_binary else "python3"
+            verbose = " --verbose" if args.verbose else "" 
+            cmd = ["ssh"] + ssh_options + shlex.split("-R %(port)s:localhost:%(port)s sudo %(python_exec)s -%(verbose)s --remote_port %(port)s --" % locals())
+            log(cmd)
+            
+            import tornado.process as tornado_process
+            Subprocess = tornado_process.Subprocess
+            
+            if True:
+                ssh_proc = Subprocess(cmd, stdin=subprocess.PIPE)
+                
+                with open(__file__, "rb") as f:
+                    this_text = f.read()
+                # :TODO: если на той стороне не скачают, то будет затык
+                # => лучше асинхронно сделать
+                ssh_proc.stdin.write(this_text)
+                ssh_proc.stdin.close()
+            else:
+                cmd = ["ssh"] + ssh_options + shlex.split("-- ls -l")
+                log(cmd)
+                ssh_proc = Subprocess(cmd)
         else:
             def on_connection(stream):
                 start_proxying(stream)
@@ -246,4 +307,69 @@ if __name__ == "__main__":
         #call_cmd("ssh bbl-ott-node02-01-77 sudo ls /root")
         #setup_masquerade()
         append_dns_nameserver("tun1")
+        
+    if False:
+        import sys, os
+        #print(os.environ["PS1"], "!")
+        #ret = call_cmd("/bin/bash")
+        #ret = call_cmd("mysql -u root")
+        ret = call_cmd("ssh bbl-ott-node01-01-77 bash")
+        print("!", ret)
+        
+    if True:
+        zip_path = "/home/ilya/opt/programming/ssh-inet/tornado322.zip"
+        import zipfile
+        import imp
+        with zipfile.ZipFile(zip_path) as zf:
+            
+            
+            class MemoryZipImporter:
+                def __init__(self, zf):
+                    self.zf = zf
+                    self.modules = {}
+                    
+                    for info in zf.infolist():
+                        filename = info.filename
+                        head, sep, tail = filename.rpartition(".")
+                        # поддержка только py-модулей, ради простоты
+                        if tail == "py":
+                            head_head, init_sep, tail = head.rpartition("/__init__")
+                            if init_sep and not tail:
+                                # это __init__.py
+                                head = head_head
+                            self.modules[head.replace("/", ".")] = filename
+                            
+                    #print(self.modules)
+                    
+                def find_module(self, fullname, path=None):
+                    return self if fullname in self.modules else None
+                
+                def load_module(self, fullname):
+                    try:
+                        mpath = self.modules[fullname]
+                    except:
+                        raise ImportError(fullname)
+                    
+                    mod = sys.modules.setdefault(fullname, imp.new_module(fullname))
+                    mod.__file__ = "MemoryZipImporter:%(mpath)s" % locals()
+                    # :TRICKY: для вложенных модулей на модуль родителя нужно проставить
+                    # __path__ (придет аргумент path в load_module), иначе не загрузятся
+                    mod.__path__ = [mpath]
+                    
+                    txt = zf.read(mpath)
+                    #exec txt in mod.__dict__
+                    code = compile(txt, mod.__file__, "exec")
+                    exec(code, mod.__dict__)
+
+                    return mod
+    
+            import sys
+                
+            z_importer = MemoryZipImporter(zf)
+            sys.meta_path.insert(0, z_importer)
+    
+            import tornado.ioloop
+            import tornado
+            import tornado.ioloop
+            import tornado.ioloop
         
